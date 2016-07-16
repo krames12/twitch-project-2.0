000e5198b1f6f22fbc6c1a1206a8ec836634a9ec
Nick Kramer <krames12@gmail.com>
2016-07-16 11:21:49 -0400

    from hyperdev to local / repo

---
+++ scripts.js
@@ -0,0 +1,147 @@
+/* Twitch Stream List Rewrite
+ Originally coded for FreeCodeCamp and hosted on Codepen.io 
+ Original Link: http://codepen.io/krames12/pen/EKVjxq
+
+User Stories {
+  - I can see whether my favorite user is currently streaming on Twitch.tv.
+  - I can click the status output and be sent directly to the user's Twitch.tv channel.
+  - If a Twitch user is currently streaming, I can see additional details about what they are 
+    streaming.
+  - I will see a placeholder notification if a streamer has closed their Twitch account \
+    (or the account never existed).
+    }
+
+Purpose of rewrite: {
+  - Use vanilla JS to handle things instead of jQuery.
+  - Learn to use Object Oriented Programming to easily group and handle things in a much cleaner and easier to reason about manner.
+  
+}
+
+Object Planning Structure {
+  - send request
+  	- send the request and return data
+  - view
+  	- refresh / display list
+  	- delete item
+  - handlers
+  	- grabs input and passes to request
+  	- delete stream item
+  - process the stream data
+  	- check if account exists
+  	- check if online          ---- this section can be taken care of in the add by using an object per streamer.
+  	- get game info
+  	- get profile picture
+  - stream list check and handler
+  	- list itself
+  	- add to list
+  	- delete from list
+  	- check if on list already
+}
+*/
+
+var streamerList = {
+  streamers: [],
+  
+  addStream: function(isOnline, streamName, streamData) {
+    if(isOnline === true){
+    this.streamers.push({
+      streamName: streamName,
+      currentGame: streamData.stream.channel.game,
+      streamUrl: 'https://twitch.tv/' + streamName,
+      profilePicture: streamData.stream.channel.logo
+    });
+    } else {
+      this.streamers.push({
+       streamName: streamName,
+       streamUrl: 'https://twitch.tv/' + streamName,
+      });
+    }
+    
+    view.displayStreams();
+  },
+  
+  deleteStream: function(position) {
+    this.streamers.splice(position, 1);
+    view.displayStreams();
+  },
+  
+  streamStatus: function(streamData) {
+    if(streamData.stream === true) {
+      return true;
+    } else {
+      return false;
+    }
+  },
+  
+  checkDuplicateStream: function(streamData) {}
+}
+
+// Should pass in this.onSuccess for the callback
+var twitchRequest = {
+  twitchCall: function(streamName, callback){
+    var data;
+    var request = new XMLHttpRequest();
+    request.open('GET', 'https://api.twitch.tv/kraken/streams/' + streamName + '?', true);
+
+    request.onload = function() {
+      if (request.status >= 200 && request.status < 400) {
+        data = JSON.parse(request.responseText);
+				console.log(data);
+        callback(null, data);
+      }
+    };
+
+    request.onerror = function() {
+      console.log('error');
+      callback('error', null);
+    };
+
+    request.send();
+  },
+  
+	// the jQuery way, which is doing the exact same thing response time wise.
+  //twitchCalled: function(streamName){
+  //  $.ajax({
+  //    url: 'https://api.twitch.tv/kraken/streams/' + streamName + '?',
+  //    datatype: 'jsonp',
+  //    success: this.onSuccess(data),
+  //    error: function(){
+  //      console.log('error! error!');
+  //    }
+  //  });
+  //},
+	
+	onSuccess: function(error, data){
+	  if(data === nul){
+	    console.log('error');
+	  } else {
+	    console.log(data);
+	  }
+	}
+};
+
+
+var view = {
+  displayStreams: function(){
+    var streamDocumentList = document.querySelector('ul');
+    console.log(streamerList.streamers);
+  }
+}
+
+var handlers = {
+  addStreamInput: function(streamName){
+    var streamData = twitchRequest.twitchCall(streamName);
+    
+    var streamState;
+    
+    if(data.stream === null){
+      streamState = false;
+    } else {
+      streamState = true;
+    }
+    console.log(streamState);
+    streamerList.addStream(streamState, streamName, streamData);
+  },
+ 
+  deleteStreamItem: function(position) {}
+}
