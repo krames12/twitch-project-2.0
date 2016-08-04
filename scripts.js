55f5c269fd1c81ca7ae61b9890f6c77b5ebb0403
Nick Kramer <krames12@gmail.com>
2016-08-04 00:57:29 -0400

    deleted attempt at small 'online' animation and comments that were not needed

--- scripts.js
+++ scripts.js
@@ -1,44 +1,4 @@
-/* Twitch Stream List Rewrite
- Originally coded for FreeCodeCamp and hosted on Codepen.io 
- Original Link: http://codepen.io/krames12/pen/EKVjxq
-
-User Stories {
-  - I can see whether my favorite user is currently streaming on Twitch.tv.
-  - I can click the status output and be sent directly to the user's Twitch.tv channel.
-  - If a Twitch user is currently streaming, I can see additional details about what they are 
-    streaming.
-  - I will see a placeholder notification if a streamer has closed their Twitch account \
-    (or the account never existed).
-    }
-
-Purpose of rewrite: {
-  - Use vanilla JS to handle things instead of jQuery.
-  - Learn to use Object Oriented Programming to easily group and handle things in a much cleaner and easier to reason about manner.
-  
-}
-
-Object Planning Structure {
-  - send request
-  	- send the request and return data
-  - view
-  	- refresh / display list
-  	- delete item
-  - handlers
-  	- grabs input and passes to request
-  	- delete stream item
-  - process the stream data
-  	- check if account exists
-  	- check if online          ---- this section can be taken care of in the add by using an object per streamer.
-  	- get game info
-  	- get profile picture
-  - stream list check and handler
-  	- list itself
-  	- add to list
-  	- delete from list
-  	- check if on list already
-}
-*/
-
+// Anything to do with streamer array directly
 var streamerList = {
   streamers: [],
   
